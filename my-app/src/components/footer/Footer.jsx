5f0a49dd7b6f199be9a2df1c2c8469399f5c633e
anas <anasoudsassi@gmail.com>
2024-07-03 16:47:53 +0100

    adding footer component

---
+++ my-app/src/components/footer/Footer.jsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import styles from './footer.module.css'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+function Footer() {
+  const pathname = usePathname()
+  const signPage = ['/signin', '/signup'].includes(pathname);
+
+  return (
+    <>
+    {!signPage && <div className={styles.footer}>
+      <p>&copy;2024 All rights reserved</p>
+      <div>
+        <a target="_blank" href='https://github.com/Anasoud1/'>
+          <Image className={styles.icon} src="/img/github.png" width={20} height={20} alt="github"/>
+        </a>
+        
+        <a target="_blank" href='https://www.linkedin.com/in/anas-oudsassi/'>
+          <Image className={styles.icon} src="/img/linkedin.png" width={20} height={20} alt="linkedin"/>
+        </a>
+
+      </div>
+      
+      
+    </div>}
+    </>
+    
+  )
+}
+
+export default Footer
