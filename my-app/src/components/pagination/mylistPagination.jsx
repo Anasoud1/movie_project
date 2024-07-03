03706f0f350131fa7f939f9c1ba0a426dbe3f1d2
anas <anasoudsassi@gmail.com>
2024-07-03 16:51:42 +0100

    adding pagination component

---
+++ my-app/src/components/pagination/mylistPagination.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './pagination.module.css'
+
+function MylistPagination({totalPosts, postPerPage, currentPage, setCurrentPage}) {
+  let pages = []
+
+  for(let i = 1; i <= Math.ceil(totalPosts/postPerPage); i++){
+    pages.push(i)
+  }
+  console.log('totalPosts: ',totalPosts)
+  console.log('pages: ',pages)
+
+  return (
+    <>
+    {pages.length > 1 && <div className={styles.pagination}>
+    {
+        pages.map(page => (
+            <button key={page} className={`${styles.btn} ${currentPage === page ? styles.active : null}`} onClick={() =>{setCurrentPage(page)}}>{page}</button>
+        ))
+    }
+    </div>}
+    </>
+    
+  )
+}
+
+export default MylistPagination
---
+++ my-app/src/components/pagination/pagination.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './pagination.module.css'
+function Pagination({currentPage, setCurrentPage}) {
+    let pages = []
+
+    for (let i = 1; i <= 10; i++){
+        pages.push(i);
+    }
+  return (
+    <div className={styles.pagination}>
+        {
+            pages.map(page => (
+                <button key={page} className={`${styles.btn} ${page === currentPage? styles.active: ''}`} onClick={()=>{setCurrentPage(page)}}>{page}</button>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Pagination
