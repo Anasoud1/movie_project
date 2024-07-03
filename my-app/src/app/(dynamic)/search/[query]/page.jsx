e4a2f976a0ad41620fb2c0cb8508d2ff03dcc1a4
anas <anasoudsassi@gmail.com>
2024-07-03 17:00:29 +0100

    adding /search page

---
+++ my-app/src/app/(dynamic)/search/[query]/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Posts from '@/components/posts/posts';
+
+export const metadata = {
+  title: "YouMovie - Search",
+  description: "Find your movie",
+};
+
+const apiKey = process.env.API_KEY;
+
+function SearchPage({params}) {
+  const searchText = params.query;
+  console.log('searchText: ', searchText)
+  const url = 'https://api.themoviedb.org/3/search/movie?query=' + searchText + "&api_key=" + apiKey
+
+  return (
+    <div>
+      <Posts apiUrl={url}/>
+    </div>
+  )
+}
+
+export default SearchPage
