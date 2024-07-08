1043dac7c60c8145875198446bfb72636ac21d30
anas <anasoudsassi@gmail.com>
2024-07-08 18:35:03 +0100

    removing unnecessary code

--- my-app/src/app/hooks/useAuth.js
+++ my-app/src/app/hooks/useAuth.js
@@ -2,14 +2,11 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../firebase"; 
-// import { usePathname } from "next/navigation";
-// import { useRouter } from "next/navigation";
+
 
 function useAuth() {
     const [localUser, setLocalUser] = useState(null);
-    // const pathname = usePathname()
-    // const router = useRouter()
-
+ 
     useEffect(() => {
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
