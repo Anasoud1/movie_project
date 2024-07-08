5fa208c3ab000462d4a4bc3e5126ff1f0a459dc8
anas <anasoudsassi@gmail.com>
2024-07-08 23:16:37 +0100

    Set up environment variables

--- my-app/src/app/firebase.js
+++ my-app/src/app/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4iZlx6Kh_mGR7sSBTRZtt5ytczX9sZXI",
-  authDomain: "my-app-6f58c.firebaseapp.com",
-  projectId: "my-app-6f58c",
-  storageBucket: "my-app-6f58c.appspot.com",
-  messagingSenderId: "632576460346",
-  appId: "1:632576460346:web:e20d761ab9f05e212a1703"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
